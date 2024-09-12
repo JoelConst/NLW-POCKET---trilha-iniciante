577333a717d769054d2c6bfb5115ede4c0bed6df
joelConst <joel_leite1@hotmail.com>
2024-09-12 20:29:50 -0300

    funcionalidades

---
+++ index.js
@@ -0,0 +1,13 @@
+let meta = {
+  value: 'ler um livro por mes',
+  checked: true,
+};
+
+let metas = [
+  meta,
+  {
+    value: 'caminhar 20 minutos todos os dias',
+    checked: false,
+  },
+];
+console.log(metas[1].value);

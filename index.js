ea48615a40f5a99b133dc389da87395705c23cc8
joelConst <joel_leite1@hotmail.com>
2024-09-16 23:30:14 -0300

    lista de metas

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-const { select } = require('@inquirer/prompts');
+const { select, input, checkbox } = require('@inquirer/prompts');
+
+let meta = {
+  value: 'tomar 3L de agua por dia',
+  checked: false,
+};
+
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+  const meta = await input({
+    message: 'Digite a meta:',
+  });
+
+  if (meta.length == 0) {
+    console.log(' A Meta nao pode ser vazia!');
+    return;
+  }
+  metas.push({
+    value: meta,
+    checked: false,
+  });
+};
+
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message:
+      'Use as setas para mudar de meta o espaço para marcar ou desmarcar, e o Enter para finalizar essa etapa:',
+    choices: [...metas],
+    instructions: false,
+  });
+  if (respostas.length == 0) {
+    console.log('Você não marcou nenhuma meta!');
+    return;
+  }
+
+  metas.forEach(m => {
+    m.checked = false;
+  });
+
+  respostas.forEach(resposta => {
+    const meta = metas.find(m => {
+      return m.value == resposta;
+    });
+    meta.checked = true;
+  });
+  console.log('Meta(s) marcadas como concluida(s)');
+};
 
 const start = async () => {
   while (true) {
@@ -7,12 +54,9 @@ const start = async () => {
       choices: [
         {
           name: 'cadastrar meta',
-          value: 'sair',
+          value: 'cadastrar',
         },
-        {
-          name: 'sair',
-          value: 'sair',
-        },
+
         {
           name: 'listar Metas',
           value: 'listar',
@@ -26,14 +70,17 @@ const start = async () => {
 
     switch (opcao) {
       case 'cadastrar':
-        console.log('vamos cadastrar');
+        await cadastrarMeta();
+        console.log(metas);
 
         break;
       case 'listar':
+        await listarMetas();
         console.log('vamos Listar');
-
         break;
+
       case 'sair':
+        console.log('ate a proxima...!');
         return;
     }
   }
